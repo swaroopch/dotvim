614b1673f462082e2474b51a1e9949b23322235f
Swaroop C H <swaroop@swaroopch.com>
2011-07-27 21:51:24 +0530

    Adding a if guard for idempotency

--- install.sh
+++ install.sh
@@ -20,8 +20,11 @@ then
 fi
 
 # Download upstart.vim
-mkdir -p ~/.vim/syntax/
-curl http://bazaar.launchpad.net/~upstart-devel/upstart/trunk/download/head:/upstart.vim-20090708195914-1n7k3bcwobwm4ag7-7/upstart.vim -o ~/.vim/syntax/upstart.vim
+if [[ ! -f ~/.vim/syntax/upstart.vim ]]
+then
+    mkdir -p ~/.vim/syntax/
+    curl http://bazaar.launchpad.net/~upstart-devel/upstart/trunk/download/head:/upstart.vim-20090708195914-1n7k3bcwobwm4ag7-7/upstart.vim -o ~/.vim/syntax/upstart.vim
+fi
 
 # vimrc, gvimrc
 ln -s -f $PWD/vimrc ~/.vimrc
