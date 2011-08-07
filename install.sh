015aab816ad26ad876b9e2a80a911be8c99ab47f
Swaroop C H <swaroop@swaroopch.com>
2011-08-07 11:13:08 +0530

    Replacing ~ with $HOME and other formatting changes

--- install.sh
+++ install.sh
@@ -12,23 +12,26 @@ then
     exit 1
 fi
 
-#rm -rf ~/.vimrc ~/.gvimrc  ~/.vimrc.local ~/.vim
+echo "Backup existing vim files"
+export VIM_BACKUP_DIR="/tmp/dotvim-backup"
+mkdir -p $VIM_BACKUP_DIR
+for f in $(ls -a $VIM_BACKUP_DIR| grep -v '^.$' | grep -v '^..$')
+do
+    rm -rf "$VIM_BACKUP_DIR/$f"
+done
+for f in "$HOME/.vimrc" "$HOME/.gvimrc" "$HOME/.vimrc.local" "$HOME/.vim"
+do
+    [[ -s "$f" ]] && mv -vf "$f" $VIM_BACKUP_DIR
+done
 
-# Download Vundle
-mkdir -p ~/.vim/bundle
-if [[ ! -d ~/.vim/bundle/vundle ]]
+echo "Download Vundle"
+mkdir -p "$HOME/.vim/bundle"
+if [[ ! -d "$HOME/.vim/bundle/vundle" ]]
 then
-    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    git clone http://github.com/gmarik/vundle.git "$HOME/.vim/bundle/vundle"
 fi
 
-# Download upstart.vim
-if [[ ! -f ~/.vim/syntax/upstart.vim ]]
-then
-    mkdir -p ~/.vim/syntax/
-    curl http://bazaar.launchpad.net/~upstart-devel/upstart/trunk/download/head:/upstart.vim-20090708195914-1n7k3bcwobwm4ag7-7/upstart.vim -o ~/.vim/syntax/upstart.vim
-fi
-
-# Link vimrc, gvimrc
+echo "Link vimrc, gvimrc"
 ln -s -f "$PWD/vimrc" "$HOME/.vimrc"
 ln -s -f "$PWD/gvimrc" "$HOME/.gvimrc"
 if [[ -f "$PWD/vimrc.local" ]]
@@ -36,32 +39,39 @@ then
     ln -s -f "$PWD/vimrc.local" "$HOME/.vimrc.local"
 fi
 
-# Tell Vundle to download all the scripts
+echo "Instruct Vundle to download all the scripts"
 vim -c "BundleInstall" -c "quit"
 
-# Command-T post-download installation
-if [[ -d ~/.vim/bundle/command-t ]]
+echo "Download upstart.vim"
+if [[ ! -f $HOME/.vim/syntax/upstart.vim ]]
 then
-    cd ~/.vim/bundle/command-t
+    mkdir -p $HOME/.vim/syntax/
+    curl "http://bazaar.launchpad.net/~upstart-devel/upstart/trunk/download/head:/upstart.vim-20090708195914-1n7k3bcwobwm4ag7-7/upstart.vim" -o "$HOME/.vim/syntax/upstart.vim"
+fi
+
+echo "Command-T post-download installation"
+if [[ -d "$HOME/.vim/bundle/command-t" ]]
+then
+    cd "$HOME/.vim/bundle/command-t"
     rake make
     cd -
 fi
 
-# Custom snippets
-if [[ -d ~/.vim/bundle/snipmate.vim ]]
+echo "Custom snippets"
+if [[ -d "$HOME/.vim/bundle/snipmate.vim" ]]
 then
-    cat snippets/eruby.snippets  >> ~/.vim/bundle/snipmate.vim/snippets/eruby.snippets
-    cat snippets/python.snippets >> ~/.vim/bundle/snipmate.vim/snippets/python.snippets
+    cat snippets/eruby.snippets  >> "$HOME/.vim/bundle/snipmate.vim/snippets/eruby.snippets"
+    cat snippets/python.snippets >> "$HOME/.vim/bundle/snipmate.vim/snippets/python.snippets"
 fi
 
-# Jinja post-download installation
-if [[ -d ~/.vim/bundle/Jinja ]] && [[ ! -f ~/.vim/bundle/Jinja/syntax/htmljinja.vim ]]
+echo "Jinja post-download installation"
+if [[ -d "$HOME/.vim/bundle/Jinja" ]] && [[ ! -f "$HOME/.vim/bundle/Jinja/syntax/htmljinja.vim" ]]
 then
-    curl http://www.vim.org/scripts/download_script.php?src_id=6961 -o ~/.vim/bundle/Jinja/syntax/htmljinja.vim
+    curl "http://www.vim.org/scripts/download_script.php?src_id=6961" -o "$HOME/.vim/bundle/Jinja/syntax/htmljinja.vim"
 fi
 
-# Remove explicit Safari mention
-if [[ -d ~/.vim/bundle/Textile-for-VIM/ ]]
+echo "Remove explicit Safari mention in Textile-for-VIM"
+if [[ -d "$HOME/.vim/bundle/Textile-for-VIM/" ]]
 then
     sed -i.bak 's/open -a Safari/open/' "$HOME/.vim/bundle/Textile-for-VIM/ftplugin/textile.vim"
 fi
